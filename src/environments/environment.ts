228e2f66b46fe236e3ce7ad00a55e11b3e34a81d
MohitKasnia9 <luffysanpvt@gmail.com>
2025-02-07 15:08:48 +0530

    Delete src/environments directory

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,          // Set to true for production environment
-    apiUrl: 'http://localhost:3000', 
-    // // The backend API URL (adjust if needed)
-    jwtSecretKey: 'secretKey',
-  };
-  
